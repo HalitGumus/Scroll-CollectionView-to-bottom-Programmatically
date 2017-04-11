616fc366b56f04e5f7e27d556cf82b1a52984a4f
Halit GUMUS <halit608@hotmail.com>
2017-04-11 10:54:01 +0300

    Update example.swift

--- example.swift
+++ example.swift
@@ -1,1 +1,11 @@
-import UIKit
+let x = yourArray.count - 1
+
+let y = NSIndexPath(item: x, section: 0)
+
+collectionView.scrollToItem(at: y as IndexPath, at: .bottom, animated: true)
+
+OR
+
+collectionView.contentOffset.y = (400)
+
+self.collectionView.scrollToItemAtIndexPath(NSIndexPath(forItem: yourArray.count, inSection: 0), atScrollPosition: .Bottom, animated: true)
